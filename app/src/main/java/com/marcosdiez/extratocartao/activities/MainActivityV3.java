dc4a7758a68b61f0c8a59d20e1bea2c210119736
Marcos Diez <marcos@unitron.com.br>
2016-02-07 17:24:52 +0000

    one can now see all expenses from this card/store/bank

--- app/src/main/java/com/marcosdiez/extratocartao/activities/MainActivityV3.java
+++ app/src/main/java/com/marcosdiez/extratocartao/activities/MainActivityV3.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -53,8 +54,14 @@ public class MainActivityV3 extends AppCompatActivity {
 
     private void initListView() {
         purchaseListView = (ListView) findViewById(R.id.purchase_list);
+        registerForContextMenu(purchaseListView);
 
         List<Purchase> pList = Purchase.find(Purchase.class, null, null, null, "id desc", null);
+        populatListView(pList);
+
+    }
+
+    private void populatListView(List<Purchase> pList) {
         double total = 0;
         for (Purchase p : pList) {
             total += p.getAmount();
@@ -67,16 +74,83 @@ public class MainActivityV3 extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Purchase p = (Purchase) purchaseListView.getAdapter().getItem(position);
-                Toast.makeText(mySelf, p.toString(), Toast.LENGTH_LONG).show();
-
-                if (p.hasMap()) {
-                    Util.openUrl(p, mySelf);
-                }
+                openPurchaseUrlIfPossible(p);
 
             }
         });
     }
 
+    private void openPurchaseUrlIfPossible(Purchase p) {
+        Toast.makeText(mySelf, p.toString(), Toast.LENGTH_LONG).show();
+
+        if (p.hasMap()) {
+            Util.openUrl(p, mySelf);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View theView,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        if (theView.getId() == R.id.purchase_list) {
+//            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+  //          Purchase p = (Purchase) purchaseListView.getAdapter().getItem(info.position);
+            getMenuInflater().inflate(R.menu.context_menu, menu);
+        }
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo());
+        Purchase thePurchase = (Purchase) purchaseListView.getAdapter().getItem(info.position);
+        switch (item.getItemId()) {
+            case R.id.action_open_in_map:
+                openPurchaseUrlIfPossible(thePurchase);
+                return true;
+            case R.id.action_show_only_this_card:
+                // edit stuff here
+                show_only_this_credit_card(thePurchase);
+                return true;
+            case R.id.action_show_only_this_store:
+                show_only_this_store(thePurchase);
+                return true;
+
+            case R.id.action_show_only_this_bank:
+                show_only_this_bank(thePurchase);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void show_only_this_bank(Purchase thePurchase) {
+        String theQuery = "select PURCHASE.* \n" +
+                "FROM PURCHASE \n" +
+                "JOIN CARD ON CARD.ID = PURCHASE.CARD\n" +
+                "JOIN BANK ON BANK.ID = CARD.BANK\n" +
+                "WHERE BANK.ID = ? \n" +
+                "ORDER BY ID DESC";
+
+        String[] parameters = {thePurchase.getCard().getBank().getId().toString()};
+        List<Purchase> pList3 = Purchase.findWithQuery(Purchase.class, theQuery, parameters);
+        populatListView(pList3);
+    }
+
+    private void show_only_this_store(Purchase thePurchase) {
+        String[] parameters2 = {thePurchase.getStore().getId().toString()};
+        List<Purchase> pList2 = Purchase.find(Purchase.class,
+                "store = ?", parameters2,
+                null, "id desc", null);
+        populatListView(pList2);
+    }
+
+    private void show_only_this_credit_card(Purchase thePurchase) {
+        String[] parameters = {thePurchase.getCard().getId().toString()};
+        List<Purchase> pList = Purchase.find(Purchase.class,
+                "card = ?", parameters,
+                null, "id desc", null);
+        populatListView(pList);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,6 +158,8 @@ public class MainActivityV3 extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main_activity_v3, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -123,10 +199,8 @@ public class MainActivityV3 extends AppCompatActivity {
 
 
     void showDialog() {
-        DialogFragment newFragment = AboutDialogFragment.newInstance(
-                R.string.action_about);
+        DialogFragment newFragment = AboutDialogFragment.newInstance(R.string.action_about);
         newFragment.show(getFragmentManager(), "dialog");
     }
-
 
 }
