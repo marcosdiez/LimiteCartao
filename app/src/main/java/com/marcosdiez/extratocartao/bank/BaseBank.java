1bf86a06f6f60f582af3b1b28994f26af291cdf6
Marcos Diez <marcos@unitron.com.br>
2018-04-01 07:39:24 +0000

    bradesco parcelado

--- app/src/main/java/com/marcosdiez/extratocartao/bank/BaseBank.java
+++ app/src/main/java/com/marcosdiez/extratocartao/bank/BaseBank.java
@@ -13,16 +13,18 @@ import java.util.regex.Pattern;
 public abstract class BaseBank {
     private static String TAG = "EC-Bank";
 
+    protected final String diaMesAno = "(\\d+/\\d+/\\d+)";
+    protected final String diaMesAnoHoraMinuto = "(\\d+/\\d+/\\d+\\s+\\d+:\\d+)";
+    protected final String valor = "(\\d+[\\.\\d]?\\d*,\\d+)";
+
     protected SMSData lastSms;
     protected Matcher lastMatch;
     private Pattern pattern = Pattern.compile(getRegEx(), Pattern.CASE_INSENSITIVE);
-    ;
 
     public boolean isSmsFromBank(SMSData theSms) {
         lastSms = theSms;
         lastMatch = pattern.matcher(theSms.getBody());
         return lastMatch.find();
-
     }
 
     protected abstract String getRegEx();
--- app/src/main/java/com/marcosdiez/extratocartao/bank/BradescoCredito.java
+++ app/src/main/java/com/marcosdiez/extratocartao/bank/BradescoCredito.java
@@ -8,9 +8,10 @@ import com.marcosdiez.extratocartao.sms.BankSms;
 public class BradescoCredito extends BaseBank {
 
     // BRADESCO CARTOES: COMPRA APROVADA NO CARTAO FINAL 5761 EM 30/07/2015 21:02. VALOR DE $ 41,00 NO(A) PANINI PIZZA.             SAO PAULO.
-
+    // BRADESCO CARTOES: COMPRA APROVADA NO CARTAO FINAL 2561 EM 29/03/2018 07:01 NO VALOR DE R$ 1.069,14 EM 6 X NO(A) DUFRY LOJAS FRANCAS LT   GUARULHOS.";
     protected String getRegEx() {
-        return "(\\w+)\\s+CARTOES:\\s+COMPRA\\s+APROVADA\\s+NO\\s+CARTAO\\s+FINAL\\s+(\\d+)\\s+EM\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+)\\.?\\s*(NO)?\\s+VALOR\\s+DE\\s+\\$?\\s+(\\d+[\\.\\d]?\\d*,\\d+)\\s+(EM\\s+\\d+\\s*X\\s*)?NO\\(A\\)\\s+(.+)\\.";
+        return "(\\w+)\\s+CARTOES:\\s+COMPRA\\s+APROVADA\\s+NO\\s+CARTAO\\s+FINAL\\s+(\\d+)\\s+EM\\s+" +
+                diaMesAnoHoraMinuto + "\\.?\\s*(NO)?\\s+VALOR\\s+DE\\s+R?\\$?\\s*" + valor + "\\s+(EM\\s+\\d+\\s*X\\s*)?NO\\(A\\)\\s+(.+)\\.";
     }
 
     public BankSms getBankSmsHelper() {
